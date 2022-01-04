31217704b75b8c7cdc2b888e571bbed5a08e8786
bezouska.jan.2018 <bezouska.jan.2018@UC313PC17>
2022-01-04 14:52:24 +0100

    ez

--- Program.cs
+++ Program.cs
@@ -4,11 +4,52 @@ namespace MonteCarlo
 {
     class Program
     {
+        static double allHits = 100000000;
+        static int a = 1;
+        static int b = 3;
+        static int maxY = 8;
+        static Random rnd = new Random();
+
         static void Main(string[] args)
         {
+            Calculate();
+        }
 
+        static void Calculate()
+        {
+            OneVariableFunction f = TestFunction;
+            double bigSurface = Math.Abs(a-b)*maxY;
+            double smallSurface;
+            double x;
+            double y;
+            double successfulHits = 0;
+            double successfulHitsRatio;
+
+            for (int i = 0; i < allHits; i++)
+            {
+                x = rnd.Next(a, b) + rnd.NextDouble();
+                y = rnd.Next(maxY) + rnd.NextDouble();
+
+                if (f(x) >= y)
+                {
+                    successfulHits++;
+                }
+                //Console.WriteLine($"x: {x}; y: {y}; f(x): {f(x)}; successful: {f(x) <= y}");
+            }
+
+            successfulHitsRatio = successfulHits / allHits;
+            smallSurface = successfulHitsRatio * bigSurface;
+            Console.WriteLine("All hits: " + allHits);
+            Console.WriteLine("Successful hits: " + successfulHits);
+            Console.WriteLine("Ratio: " + successfulHitsRatio);
+            Console.WriteLine("Surface under function: " + smallSurface);
+        }
+
+        static double TestFunction(double x)
+        {
+            return Math.Pow(2, x);
         }
     }
 
-    public delegate double FunctionForSurface(double d);
+    public delegate double OneVariableFunction(double x);
 }
